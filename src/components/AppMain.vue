360d796c60e86fc97ef9702e2c5db6bf0acc385f
CarloCassini <c.cassini87@gmail.com>
2023-09-13 10:06:56 +0200

    fix color

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -11,7 +11,7 @@ export default {
 
 <template>
   <section>
-    <div class="container">
+    <div class="container bg-brown">
       <div class="select-box py-3">
         <select
           class="form-select form-select-lg mb-3"
@@ -23,9 +23,11 @@ export default {
           <option value="3">Three</option>
         </select>
       </div>
+    </div>
 
+    <div class="container">
       <!-- container per le card -->
-      <div class="container">
+      <div class="">
         <div class="row row-cols-5 justify-content-between">
           <div class="col my-2" v-for="(card, index) in store.cards">
             <div class="card">
@@ -49,6 +51,21 @@ export default {
 </template>
 
 <style scoped>
+.container {
+  background-color: white;
+  .card {
+    background-color: #d48f38;
+    text-align: center;
+    height: 500px;
+  }
+  .card-title {
+    margin-bottom: auto;
+    color: white;
+  }
+}
+.bg-brown {
+  background-color: #d48f38;
+}
 section {
   background-color: #d48f38;
 }
